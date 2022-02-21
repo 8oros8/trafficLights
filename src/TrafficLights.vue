dcc4fa2020d9ea0104d7f3d2bdd31d67710adb7e
Timur Karamyshev <timur0karamyshev@gmail.com>
2022-02-21 12:26:17 +0300

    Add countdown element, add flickering when lights are about to switch

--- src/TrafficLights.vue
+++ src/TrafficLights.vue
@@ -3,47 +3,93 @@
 
 export default {
   props: ['color'],
-  watch: {
-    color: function (newColor, previousColor) {
-      if (newColor === 'green') {
+  data() {
+    return {
+      timer: 0,
+    }
+  },
+  methods : {
+    redGreenHandler: function (color) {
+      if (color === 'green') {
+        let flickeringGreen1
+        let flickeringGreen2
+        this.timer = 15
+        let countdown = setInterval(() => {
+          this.timer--
+        }, 1000)
+        setTimeout(() => {
+          flickeringGreen1 = setInterval(() => {
+            document.querySelector('.greenCircle').style.opacity = '0.2'
+          }, 500)
+          flickeringGreen2 = setInterval(() => {
+            document.querySelector('.greenCircle').style.opacity = '1'
+          }, 1000)
+        }, 12000)
         setTimeout( () => {
+          clearInterval(countdown)
+          clearInterval(flickeringGreen1)
+          clearInterval(flickeringGreen2)
           this.$router.push('/yellow')
         }, 15000)
       }
-      if (newColor === 'yellow') {
-        if (previousColor === 'green') {
-          setTimeout( () => {
-            this.$router.push('/red')
-          }, 3000)
-        }
-        if (previousColor === 'red') {
-          setTimeout( () => {
-            this.$router.push('/green')
-          }, 3000)
-        }
-      }
-      if (newColor === 'red') {
+      if (color === 'red') {
+        this.timer = 10
+        let countdown = setInterval(() => {
+          this.timer--
+        }, 1000)
+        let flickeringRed1
+        let flickeringRed2
+        setTimeout(() => {
+          flickeringRed1 = setInterval(() => {
+            document.querySelector('.redCircle').style.opacity = '0.2'
+          }, 500)
+          flickeringRed2 = setInterval(() => {
+            document.querySelector('.redCircle').style.opacity = '1'
+          }, 1000)
+        }, 7000)
         setTimeout( () => {
+          clearInterval(countdown)
+          clearInterval(flickeringRed1)
+          clearInterval(flickeringRed2)
           this.$router.push('/yellow')
         }, 10000)
       }
     }
   },
-  created() {
-    if (this.$route.params.color === 'green') {
-        setTimeout( () => {
-          this.$router.push('/yellow')
-        }, 15000)
+  watch: {
+    color: function (newColor, previousColor) {
+      this.redGreenHandler(newColor)
+      if (newColor === 'yellow') {
+        this.timer = 3
+        let countdown = setInterval(() => {
+          this.timer--
+        }, 1000)
+        if (previousColor === 'green') {
+          setTimeout( () => {
+            clearInterval(countdown)
+            this.$router.push('/red')
+          }, 3000)
+        }
+        if (previousColor === 'red') {
+          setTimeout( () => {
+            clearInterval(countdown)
+            this.$router.push('/green')
+          }, 3000)
+        }
+      }
     }
+  },
+  created() {
+    this.redGreenHandler(this.$route.params.color)
     if (this.$route.params.color === 'yellow') {
+      this.timer = 3
+      let countdown = setInterval(() => {
+        this.timer--
+      }, 1000)
       setTimeout( () => {
+        clearInterval(countdown)
         this.$router.push('/red')
       }, 3000)
-    }
-    if (this.$route.params.color === 'red') {
-      setTimeout( () => {
-        this.$router.push('/yellow')
-      }, 10000)
     }
   }
 }
@@ -57,6 +103,7 @@ export default {
   <div v-else class="yellowCircle"></div>
   <div v-if="color === 'green'" class="greenCircle enabled"></div>
   <div v-else class="greenCircle"></div>
+  <div class="timer">{{color}}: {{timer}}</div>
 </template>
 
 <style>
@@ -72,6 +119,19 @@ body {
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+}
+
+.timer {
+  display: flex;
+  background-color: white;
+  height: 8vh;
+  width: 25vh;
+  font-size: 5vh;
+  border-radius: 10px;
+  border-width: thick;
+  justify-content: center;
+  align-items: center;
+  font-family: sans-serif;
 }
 
 .greenCircle {
